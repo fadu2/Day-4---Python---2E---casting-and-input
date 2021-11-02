95587c67bb810d58aef5fce1b8b6f01f50e273e7
FrancisOhemengA <93487638+fadu2@users.noreply.github.com>
2021-11-02 19:48:49 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,64 @@
+# a = "212"
+# b = "288"
+# c = "1100"
+
+# print(a, b, c, sep="-")
+
+# # Casting or converting from one data to another.
+# # convert 4.0 to an integer using int()
+# a = 4.0
+# print(int(a))  # 4
+
+# b = 3.14
+# print(int(b))  # 3
+
+# c = 7.9999
+# print(int(c))  # 7
+
+# # float()  for converting to float.
+# a = 3
+# print(float(a))  # 3.0
+
+# b = 15
+# print(float(b))  # 15.0
+
+# # str() for converting to string 
+# a = 67
+# print(str(a)) 
+# print("hello" + str(a))
+
+# # bool() for converting to boolean 
+# a = ""
+# print(bool(a))
+
+# a = 4
+# b = float(a)
+# print(type(b))  # type() to check the data type 
+
+# a = "123"
+# print(int(a))
+# print(float(a))
+
+# input() - command used to prompt a user for data or information
+
+# first_name = input("Enter your first name: ")
+# print("Your first name is " + first_name)
+
+# last_name = input("Enter your last name: ")
+# print("Your last name is " + last_name)
+
+# print("Full name is " + first_name + " " + last_name)
+
+#Get the user's age 
+# age = input("Enter your age: ")
+# print("You are " + age + " years old")
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+print("Sum is " + str(num1 + num2))
+print("Difference is " + str(num1 - num2))
+print("Product is", num1 * num2)
+print("Quotient is", num1 / num2)
+
+
+
